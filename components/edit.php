abe77c86177338e987a63dda720bed425c73cb4b
Viktor <tricika96@gmail.com>
2020-08-02 10:47:24 +0400

    add  editInformation() function

--- components/edit.php
+++ components/edit.php
@@ -1,5 +1,6 @@
 <?php
 include "../config/config.php";
+include  "../components/mainComponent.php";
 
 $id=$_POST['id'];
 $username = $_POST['username'];
@@ -8,39 +9,8 @@ $phone_number = $_POST['phone_number'];
 $address = $_POST['address'];
 
 //var_dump($id,$username,$job_title,$phone_number,$address);
+editInformation($id,$username,$job_title,$phone_number,$address);
 
-if(!empty($username)){
-    $sql="UPDATE users SET username=? WHERE id=?";
-    $statement=$pdo->prepare($sql);
-    $statement->bindValue(1,$username);
-    $statement->bindValue(2,$id);
-    $statement->execute();
-
-}
-if(!empty($job_title)){
-    $sql="UPDATE users SET job_title=? WHERE id=?";
-    $statement=$pdo->prepare($sql);
-    $statement->bindValue(1,$job_title);
-    $statement->bindValue(2,$id);
-    $statement->execute();
-
-}
-if(!empty($phone_number)){
-    $sql="UPDATE users SET phone_number=? WHERE id=?";
-    $statement=$pdo->prepare($sql);
-    $statement->bindValue(1,$phone_number);
-    $statement->bindValue(2,$id);
-    $statement->execute();
-
-}
-if(!empty($address)){
-    $sql="UPDATE users SET address=? WHERE id=?";
-    $statement=$pdo->prepare($sql);
-    $statement->bindValue(1,$address);
-    $statement->bindValue(2,$id);
-    $statement->execute();
-
-}
 
 
 header('Location:../index.php');
--- components/mainComponent.php
+++ components/mainComponent.php
@@ -222,6 +222,42 @@ function updateMedia($id,$image){
     $statement->bindValue(2, $id);
     $statement->execute();
 }
+function editInformation($id,$username,$job_title,$phone_number,$address)
+{
+    global  $pdo;
+    if(!empty($username)){
+        $sql="UPDATE users SET username=? WHERE id=?";
+        $statement=$pdo->prepare($sql);
+        $statement->bindValue(1,$username);
+        $statement->bindValue(2,$id);
+        $statement->execute();
+
+    }
+    if(!empty($job_title)){
+        $sql="UPDATE users SET job_title=? WHERE id=?";
+        $statement=$pdo->prepare($sql);
+        $statement->bindValue(1,$job_title);
+        $statement->bindValue(2,$id);
+        $statement->execute();
+
+    }
+    if(!empty($phone_number)){
+        $sql="UPDATE users SET phone_number=? WHERE id=?";
+        $statement=$pdo->prepare($sql);
+        $statement->bindValue(1,$phone_number);
+        $statement->bindValue(2,$id);
+        $statement->execute();
+
+    }
+    if(!empty($address)){
+        $sql="UPDATE users SET address=? WHERE id=?";
+        $statement=$pdo->prepare($sql);
+        $statement->bindValue(1,$address);
+        $statement->bindValue(2,$id);
+        $statement->execute();
+
+    }
+}
 
 
 
